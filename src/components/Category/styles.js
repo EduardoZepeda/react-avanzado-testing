adf7fbf3a90f52db9abde304693b63bf6056713d
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-06-26 15:13:26 -0500

    Add Categories and global styles

---
+++ src/components/Category/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Anchor = styled.a`
+    display: flex;
+    text-align: center;
+    text-decoration: none;
+    flex-direction: column;
+    width: 75px;
+`
+
+export const Image = styled.img`
+    border: 1px solid #ddd;
+    box-shadow: 0px 10px 14px rgba(0,0,0,.2);
+    border-radius: 50%;
+    height: auto;
+    overflow: hidden;
+    object-fit:cover;
+    height: 75px;
+    width: 75px;
+`
+
