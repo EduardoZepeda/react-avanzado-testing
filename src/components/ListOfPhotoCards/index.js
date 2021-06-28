925b4891af5422e16489618b20f837bc8434c048
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-06-28 18:21:38 -0500

    Enable the loading of Photo cards using GraphQL

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
+import { useQuery, gql } from "@apollo/client"
+
+const withPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`
 
 export const ListOfPhotoCards = () => {
+  const { loading, error, data } = useQuery(withPhotos);
+
+  if (error) {
+    return <h2>Something went wrong :(</h2>;
+  }
+  if (loading) {
+    return <AiOutlineLoading3Quarters size='32px'/>;
+  }
+
   return (
     <ul>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(id => <PhotoCard key={id} id={id} />)}
+      {data.photos.map((photo) => (
+        <PhotoCard key={photo.id} {...photo} />
+      ))}
     </ul>
-  )
-}
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const client = new ApolloClient({
+  uri: "https://react-avanzado-testing-eduardozepeda.vercel.app/graphql/",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('app'))
