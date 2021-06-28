d149b711d2af36a2ec4a36aee73912944c55bf00
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-06-28 17:44:09 -0500

    Externalize localStorage hook

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,19 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, setShow] = useState(false)
   const key = `like-${id}`
-  const [liked, setLiked] = useState(() => {
-    try {
-      const like = window.localStorage.getItem(key)
-      return like
-    } catch (e) {
-      return false
-    }
-  })
+  const [liked, setLiked] = useLocalStorage(key, false)
   const ref = useRef(null)
 
   useEffect(() => {
@@ -30,15 +25,6 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder
 
-  const setLocalStorage = value => {
-    try {
-      window.localStorage.setItem(key, value)
-      setLiked(value)
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
   return (
     <Article ref={ref}>
       {show && <><a href={`/detail/${id}`}>
@@ -46,7 +32,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
           <Img src={src} alt='' />
         </ImgWrapper>
                  </a>
-        <Button onClick={() => setLocalStorage(!liked)}>
+        <Button onClick={() => setLiked(!liked)}>
           <Icon size='32px' />{likes} likes!
         </Button>
                </>}
