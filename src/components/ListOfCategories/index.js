4035c0c0626ea99b33534005c529bc715c73eb11
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-06-27 19:35:25 -0500

    Add a sticky categories section

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -5,6 +5,13 @@ import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+
+  const renderList = (fixed) => (
+    <List className={fixed? 'fixed': ''}>
+      {categories.map(category => <Item key={category.id}><Category {...category} /></Item>)}
+    </List>
+    )
 
   useEffect(()=>{
     window.fetch('https://react-avanzado-testing-eduardozepeda.vercel.app/categories')
@@ -12,9 +19,19 @@ export const ListOfCategories = () => {
     .then(response=>setCategories(response))
   }, [])
 
+  useEffect(()=>{
+    const onScroll = e => {
+      const newShowFixed = window.scrollY>200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('stroll', onScroll)
+  })
+
   return (
-    <List>
-      {categories.map(category => <Item key={category.id}><Category {...category} /></Item>)}
-    </List>
+    <>
+    {renderList()}
+    {showFixed && renderList(true)}
+    </>
   )
 }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -5,6 +5,20 @@ export const List = styled.ul`
     overflow: hidden;
     width: 100%;
     margin: 12px 0;
+    &.fixed {
+        background:#fff;
+        border-radius: 60px;
+        box-shadow: 0 0 20px;
+        left: 0;
+        margin: 0 auto;
+        max-width: 400px;
+        padding: 5px;
+        position: fixed;
+        right: 0;
+        top: -20px;
+        transform: scale(0.5);
+        z-index:1;
+    }
 `
 
 export const Item = styled.li`
