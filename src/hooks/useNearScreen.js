e1d427ffe3de5519abef129fcce3d73a84349178
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-06-28 17:53:43 -0500

    Refactor useNearScreen hook

---
+++ src/hooks/useNearScreen.js
@@ -0,0 +1,20 @@
+import {useRef, useState, useEffect } from 'react'
+
+export function useNearScreen(){
+  const ref = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    Promise.resolve(typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver : import('intersection-observer'))
+    const observer = new window.IntersectionObserver((entries) => {
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(ref.current)
+  }, [ref])
+  return [show, ref]
+}
+
