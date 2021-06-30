e2b49856e3fc2146921a37cc5989c63c56c69369
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-06-30 18:38:54 -0500

    Add protected routes to App

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import { GlobalStyles } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Home } from './pages/Home'
+import { User } from './pages/User'
+import { Favs } from './pages/Favs'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
 import { Router } from '@reach/router'
 import { Detail } from './pages/Detail'
 import { Navbar } from './components/Navbar'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   const urlParams = new window.URLSearchParams(window.location.search)
@@ -19,7 +27,18 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
-      <Navbar/>
+      <UserLogged>
+        {
+              ({ isAuth }) => isAuth
+                ? <Router>
+                  <UserProfile path='/user' />
+                </Router>
+                : <Router>
+                  <LoginScreen path='/user' />
+                </Router>
+                        }
+      </UserLogged>
+      <Navbar />
     </>
   )
 }
