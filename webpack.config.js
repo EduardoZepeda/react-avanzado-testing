89d8907c909685fc6a4f503692d11d8e73b6d2b4
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-07-05 00:37:57 -0500

    Add a service worker and PWA manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+const path = require('path')
 
 module.exports = {
   mode: 'development',
@@ -9,7 +12,56 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    })
+    }),
+    new WebpackPwaManifest({
+          name: 'Petgram | Tu aplicación de mascotas',
+          short_name: 'Petgram',
+          description: 'El lugar donde compartes las fotos de tus compañeros de vida animales',
+          background_color: '#ffffff',
+          theme_color: '#2196f3',
+          crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+          icons: [
+            {
+              src: path.resolve('src/assets/img/icon.png'),
+              sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+            },
+            {
+              src: path.resolve('src/assets/img/icon.png'),
+              size: '1024x1024' // you can also use the specifications pattern
+            },
+            {
+              src: path.resolve('src/assets/img/icon.png'),
+              size: '1024x1024',
+              purpose: 'maskable'
+            }
+          ]
+        }),
+     new WorkboxWebpackPlugin.GenerateSW({
+          swDest: 'service-worker.js',
+          clientsClaim: true,
+          skipWaiting: true,
+          maximumFileSizeToCacheInBytes: 5000000,
+          runtimeCaching: [
+            {
+              urlPattern: new RegExp(
+                'https://(res.cloudinary.com|images.unsplash.com)'
+              ),
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'images'
+              }
+            },
+            {
+              urlPattern: new RegExp(
+                'https://react-avanzado-testing-b39h5jmoy-eduardozepeda.vercel.app/'
+              ),
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: 'api'
+              }
+            }
+          ]
+        })
   ],
   module: {
     rules: [{
